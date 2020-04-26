58f16e88557933dc66cbe5bb4b2d2c32e01cf6e4
Loren Pabst <lorenpabst@Lorens-MacBook-Pro.local>
2020-04-25 18:08:24 -0600

    cars move and stuff

--- entities.js
+++ entities.js
@@ -1,8 +1,42 @@
 var entities = {
-    Car: function(x, y, speed, color) {
+    Car: function(x, y, w, h, speed, color, destinationX, destinationY) {
         this.x = x;
         this.y = y;
+        this.w = w;
+        this.h = h;
         this.speed = speed;
         this.color = color;
+        this.path = [ 
+            { x: x, y: y } 
+        ];
+        this.currentPathIndex = 0;
+
+        // destination location
+        this.destinationX = destinationX;
+        this.destinationY = destinationY;
+
+        // colors of destination and path
+        this.destinationColorMap = {
+            red: '#f88',
+            blue: '#88f',
+            green: '#8f8',
+        }
+        this.pathColorMap = {
+            red: '#f88',
+            blue: '#88f',
+            green: '#8f8',
+        }
+        this.destinationColor = this.destinationColorMap[this.color];
+        this.pathColor = this.pathColorMap[this.color];
+
+        // location update function moves car along its path
+        this.update = function() {
+            if (this.currentPathIndex < this.path.length - 1) {
+                this.currentPathIndex++;
+            }
+            var newLocation = this.path[this.currentPathIndex];
+            this.x = newLocation.x;
+            this.y = newLocation.y;
+        }
     },
 }
--- game.js
+++ game.js
@@ -6,16 +6,44 @@ var Game = {
         canvas.height = canvas.clientHeight;
         
         // init data
-        var context = canvas.getContext('2d');
-        var animationFrame = 0;
-        var gameOver = false;
-        var gameRunning = true;
-        var level = 1;
-        var car = new entities.Car(100, 100, 2, 'red');
-        var mouseIsDown = false;
-        var data = { canvas, context, animationFrame, gameOver, gameRunning, level, car, mouseIsDown };
+        var data = {
+            canvas: canvas,
+            context: canvas.getContext('2d'),
+            animationFrame: 0,
+            gameOver: false,
+            gameRunning: true
+        }
+
+        // get data ready for the start of level 1
+        Game.initLevel(data, 2);
 
         // Add event listeners
+        Game.addEventListeners(data);
+
+        // start game loop
+        Game.run(data);
+    },
+
+    initLevel: function(data, level) {
+        data.level = level;
+        data.mouseIsDown = false;
+        data.selectedCar = null;
+        data.moveCars = false;
+    
+        var carsForLevel = {
+            1: [
+                new entities.Car(100, 100, 20, 20, 1, 'red', 500, 500)
+            ],
+            2: [
+                new entities.Car(100, 100, 20, 20, 1, 'red', 500, 500),
+                new entities.Car(500, 100, 20, 20, 1, 'blue', 100, 500)
+            ]
+        }
+    
+        data.cars = carsForLevel[level];
+    },
+
+    addEventListeners: function(data) {
         Game.handleMouseDownListener = function(event) {
             return Game.mouseDownListener(event, data);
         }
@@ -25,12 +53,13 @@ var Game = {
         Game.handleMouseMoveListener = function(event) {
             return Game.mouseMoveListener(event, data);
         }
+        Game.handleKeyPress = function(event) {
+            return Game.keypressListener(event, data);
+        }
         window.addEventListener('mousedown', Game.handleMouseDownListener);
         window.addEventListener('mousemove', Game.handleMouseMoveListener);
         window.addEventListener('mouseup', Game.handleMouseUpListener);
-
-        // start game loop
-        Game.run(data);
+        window.addEventListener('keypress', Game.handleKeyPress);
     },
 
     run: function(data)  {
@@ -52,28 +81,42 @@ var Game = {
     },
 
     update: function(data) {
-        var car = data.car;    
+        var cars = data.cars;    
         
-        // car.x += car.speed;
-        // car.y += car.speed;
+        // if cars should be moving, move them
+        if (data.moveCars) {
+            cars.forEach(car => car.update())
+        }
     },
 
     render: function(data)  {
         var canvas = data.canvas;
         var context = data.context;
         var level = data.level;
-        var car = data.car;
+        var cars = data.cars;
 
-        // display level
+        // display the level
         document.getElementById('level').innerText = 'Level: ' + level;
 
         // black background for canvas
         context.fillStyle = '#000000';
         context.fillRect(0, 0, canvas.width, canvas.height);
 
-        // draw car
-        context.fillStyle = car.color;
-        context.fillRect(car.x, car.y, 20, 20);
+        cars.forEach(car => {
+            // draw car's destination
+            context.fillStyle = car.destinationColor;
+            context.fillRect(car.destinationX - 5, car.destinationY - 5, car.w + 10, car.h + 10);
+    
+            // draw car's path
+            context.fillStyle = car.pathColor;
+            car.path.forEach(location => {
+                context.fillRect(location.x, location.y, car.w, car.h);
+            })
+    
+            // draw car
+            context.fillStyle = car.color;
+            context.fillRect(car.x, car.y, car.w, car.h);
+        })
     },
     
     gameOver: function(data) {
@@ -87,24 +130,85 @@ var Game = {
         context.fillText('Game Over', 200, 300);
 
         // remove event listeners
+        Game.removeEventListeners();
+    },
+
+    removeEventListeners: function() {
         window.removeEventListener('mousedown', Game.handleMouseDownListener);
         window.removeEventListener('mousemove', Game.handleMouseMoveListener);
         window.removeEventListener('mouseup', Game.handleMouseUpListener);
+        window.removeEventListener('keypress', Game.handleKeyPress);
     },
 
     mouseDownListener: function(e, data) {
-        console.log('mouse down');
-        data.mouseIsDown = true;
+        var canvas = data.canvas;
+        var cars = data.cars;
+        
+        // get mouse position
+        var rect = canvas.getBoundingClientRect();
+        var mouseX = e.clientX - rect.left;
+        var mouseY = e.clientY - rect.top;
+
+        // see if mouse is clicing on a car
+        cars.forEach(car => {
+            if (mouseX >= car.x && mouseX <= car.x + car.w && mouseY >= car.y && mouseY <= car.y + car.h) {
+                data.selectedCar = {
+                    car: car,
+                    offsetX: mouseX - car.x,
+                    offsetY: mouseY - car.y
+                }
+            }            
+        })
     },
 
     mouseMoveListener: function(e, data) {
-        if (!data.mouseIsDown) return;
-        console.log('mouse move');
+        // if we are in the 'moveCars' phase, do nothing
+        if (data.moveCars) return;
+
+        // if no car is being grabbed, do nothing
+        if (!data.selectedCar) return;
+        
+        var selectedCar = data.selectedCar;
+
+        // get mouse coordinates to track for car's path
+        var rect = canvas.getBoundingClientRect();
+        var mouseX = e.clientX - rect.left;
+        var mouseY = e.clientY - rect.top;
+        var newLocation = {
+            x: mouseX - selectedCar.offsetX,
+            y: mouseY - selectedCar.offsetY
+        }
+
+        // we don't want the car to return to the same place it has already been
+        // if new location is within 3px of any old locations, it isn't a valid new spot to drive to
+        var addToPathArray = true;
+        selectedCar.car.path.forEach(location => {
+            var xDistance = location.x - newLocation.x;
+            var yDistance = location.y - newLocation.y;
+            if(xDistance > -3 && xDistance < 3) addToPathArray = false;
+            if(yDistance > -3 && yDistance < 3) addToPathArray = false;
+        })
+        
+        // if the new location isn't too close to any old locations, add it to the path array
+        if (addToPathArray) {
+            selectedCar.car.path.push(newLocation)
+        }
     },
 
     mouseUpListener: function(e, data) {
-        console.log('mouse up');
-        data.mouseIsDown = false;
+        // if we are in the 'moveCars' phase, do nothing
+        if (data.moveCars) return;
+
+        data.selectedCar = null;
+    },
+
+    keypressListener: function(e, data) {
+        // if we are in the 'moveCars' phase, do nothing
+        if (data.moveCars) return;
+
+        if (e.code === 'Space'){
+            data.moveCars = !data.moveCars;
+        }
     }
 }
 
